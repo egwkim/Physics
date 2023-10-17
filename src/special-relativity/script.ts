a8d1609165e9c51cd87526f6704676da62451d59
Geonu Kim <35051914+egwkim@users.noreply.github.com>
2023-10-17 20:46:55 +0900

    Add right control functionality

--- src/special-relativity/script.ts
+++ src/special-relativity/script.ts
@@ -3,14 +3,27 @@ const controlDownDiv = <HTMLDivElement>document.getElementById('control-down');
 const controlRightDiv = <HTMLDivElement>(
   document.getElementById('control-right')
 );
+const observerControlDiv = <HTMLDivElement>(
+  document.getElementById('observer-control')
+);
+const objectControlDiv = <HTMLDivElement>(
+  document.getElementById('object-control')
+);
+
+interface Point {
+  x: number;
+  y: number;
+  lorentzFactor: number;
+  [key: string]: any;
+}
 
 class Point {
   /**
    * Basic object with base properties and methods
    */
+  private _vx!: number;
   x: number;
   y: number;
-  vx: number;
   lorentzFactor!: number;
   html: HTMLElement;
   objectName: string;
@@ -23,12 +36,24 @@ class Point {
     this.html = objectTypes[objectName].html.cloneNode(true) as HTMLElement;
     this.html.style.left = `${x * pixelInLightSecond}px`;
     this.html.style.top = `${y * pixelInLightSecond}px`;
+    this.html.addEventListener('click', () => {
+      selectedObject = this;
+      updateRightControl();
+    });
     screenDiv.appendChild(this.html);
   }
 
   updateLorentzFactor() {
     const relVx = (this.vx - observerVx) / (1 - this.vx * observerVx);
     this.lorentzFactor = (1 - relVx ** 2) ** -0.5;
+  }
+
+  public set vx(v: number) {
+    this._vx = validateVelocity(v);
+  }
+
+  public get vx(): number {
+    return this._vx;
   }
 }
 
@@ -81,13 +106,25 @@ const objectTypes: {
 const objectList: Point[] = [];
 const pixelInLightSecond = 150;
 
-let selectedObject: any = null;
+let selectedObject: string | Point | null = null;
 let playStatus: boolean = false;
 let playSpeed: number = 1;
 let observerVx = 0;
 let gamma: number = 1;
 let xOffset: number = 0;
 let tOffset: number = 0;
+
+function validateVelocity(velocity: number) {
+  if (velocity >= 1) {
+    return 0.99;
+  } else if (velocity <= -1) {
+    return -0.99;
+  } else if (isNaN(velocity)) {
+    return 0;
+  } else {
+    return velocity;
+  }
+}
 
 function getObjectHTML(objectName: string) {
   return document.querySelector(
@@ -97,20 +134,10 @@ function getObjectHTML(objectName: string) {
 
 let playInterval: number;
 function setObserver() {
-  let vx = Number.parseFloat(
-    prompt('observer velocity', observerVx.toString()) ?? '0.5'
-  );
-  if (isNaN(vx)) {
-    vx = 0.5;
-  }
-  vx = vx >= 1 ? 0.99 : vx <= -1 ? -0.99 : vx;
-  observerVx = vx;
-  xOffset = Number.parseFloat(prompt('x offset', xOffset.toString()) ?? '0');
-  if (isNaN(xOffset)) xOffset = 0;
-  tOffset = Number.parseFloat(prompt('t offset', tOffset.toString()) ?? '0');
-  if (isNaN(tOffset)) tOffset = 0;
+  objectControlDiv.style.display = 'none';
+  observerControlDiv.style.display = '';
 
-  gamma = 1 / (1 - vx ** 2) ** 0.5;
+  gamma = 1 / (1 - observerVx ** 2) ** 0.5;
 
   objectList.forEach((object) => {
     object.updateLorentzFactor();
@@ -144,7 +171,105 @@ function clearAll() {
   objectList.length = 0;
 }
 
-function initControls() {
+function initScreen() {
+  screenDiv.addEventListener('mousedown', (e) => {
+    let newObject: any;
+    const x = e.clientX / pixelInLightSecond;
+    const y = e.clientY / pixelInLightSecond;
+    if (typeof selectedObject === 'string' || selectedObject instanceof String)
+      switch (selectedObject) {
+        case 'spaceship':
+          newObject = new Spaceship(x, y, 0.5);
+          break;
+        case 'point':
+          newObject = new Point(x, y, 0);
+          break;
+        case 'light source':
+          newObject = new LightSource(x, y, 0);
+          break;
+        case 'light detector':
+          newObject = new LightDetector(x, y, 0);
+          break;
+        case 'clock':
+          newObject = new Clock(x, y, 0);
+          break;
+        default:
+          selectedObject = null;
+          updateRightControl();
+          console.error('Unkwonw object name: ' + selectedObject);
+      }
+    if (newObject) {
+      objectList.push(newObject);
+      selectedObject = newObject;
+      updateRightControl();
+    }
+  });
+}
+
+function initRightControl() {
+  const objectControlNames = ['x', 'y', 'vx'];
+  const observerControlNames = ['observer-vx', 'x-offset', 't-offset'];
+  objectControlNames.forEach((controlName) => {
+    const p = document.createElement('p');
+    p.innerText = controlName + ': ';
+    const input = document.createElement('input');
+    input.type = 'number';
+    input.id = controlName + '-input';
+    input.addEventListener('input', () => {
+      if (
+        selectedObject === null ||
+        typeof selectedObject === 'string' ||
+        selectedObject instanceof String ||
+        !(controlName in selectedObject)
+      ) {
+        return;
+      }
+
+      selectedObject[controlName] = validateVelocity(
+        Number.parseFloat(input.value)
+      );
+      render(0);
+    });
+    p.appendChild(input);
+    objectControlDiv.appendChild(p);
+  });
+  observerControlNames.forEach((controlName) => {
+    const p = document.createElement('p');
+    p.innerText = controlName + ': ';
+    const input = document.createElement('input');
+    input.type = 'number';
+    input.id = controlName + '-input';
+    switch (controlName) {
+      case 'observer-vx':
+        input.value = observerVx.toString();
+        input.addEventListener('input', () => {
+          observerVx = validateVelocity(Number.parseFloat(input.value));
+          render(0);
+        });
+        break;
+      case 'x-offset':
+        input.value = xOffset.toString();
+        input.addEventListener('input', () => {
+          xOffset = Number.parseFloat(input.value) ?? 0;
+          render(0);
+        });
+        break;
+      case 't-offset':
+        input.value = tOffset.toString();
+        input.addEventListener('input', () => {
+          tOffset = Number.parseFloat(input.value) ?? 0;
+          render(0);
+        });
+        break;
+    }
+
+    p.appendChild(input);
+    observerControlDiv.appendChild(p);
+  });
+  updateRightControl();
+}
+
+function initDownControl() {
   const buttonNames = objectNameList.concat([
     'set observer',
     'play/pause',
@@ -161,6 +286,7 @@ function initControls() {
     if ([2, 3, 4, 7, 8].includes(index)) {
       button.addEventListener('click', () => {
         selectedObject = null;
+        updateRightControl();
         alert('개발 중...');
       });
       return;
@@ -169,6 +295,7 @@ function initControls() {
     if (index < 5) {
       button.addEventListener('click', () => {
         selectedObject = buttonName.toCamelCase();
+        updateRightControl();
       });
       return;
     }
@@ -196,6 +323,7 @@ function initControls() {
     }
     button.addEventListener('click', () => {
       selectedObject = null;
+      updateRightControl();
       controlFunction();
     });
   });
@@ -209,34 +337,30 @@ function initControls() {
   });
 }
 
-function initScreen() {
-  screenDiv.addEventListener('mousedown', (e) => {
-    let newObject: any;
-    const x = e.clientX / pixelInLightSecond;
-    const y = e.clientY / pixelInLightSecond;
-    switch (selectedObject) {
-      case 'spaceship':
-        let vx = Number.parseFloat(prompt() ?? '0.5');
-        if (isNaN(vx)) vx = 0.5;
-        vx = vx >= 1 ? 0.99 : vx <= -1 ? -0.99 : vx;
-        newObject = new Spaceship(x, y, vx);
-        break;
-      case 'point':
-        newObject = new Point(x, y, 0);
-        break;
-      case 'light source':
-        newObject = new LightSource(x, y, 0);
-        break;
-      case 'light detector':
-        newObject = new LightDetector(x, y, 0);
-        break;
-      case 'clock':
-        newObject = new Clock(x, y, 0);
-        break;
-      default:
-    }
-    if (newObject) {
-      objectList.push(newObject);
+function updateRightControl() {
+  if (
+    selectedObject === null ||
+    typeof selectedObject === 'string' ||
+    selectedObject instanceof String
+  ) {
+    objectControlDiv.style.display = 'none';
+    observerControlDiv.style.display = 'none';
+    return;
+  }
+
+  observerControlDiv.style.display = 'none';
+  objectControlDiv.style.display = '';
+
+  // Fix selectedObject to avoid type errors
+  const _selectedObject = selectedObject;
+
+  const controlNames = ['x', 'y', 'vx'];
+  controlNames.forEach((controlName) => {
+    if (controlName in _selectedObject) {
+      const input = document.getElementById(
+        controlName + '-input'
+      ) as HTMLInputElement;
+      input.value = _selectedObject[controlName].toString();
     }
   });
 }
@@ -252,17 +376,23 @@ function render(t: number) {
   });
 }
 
-function main() {
+function init() {
   objectNameList.forEach((name) => {
     objectTypes[name.toCamelCase()] = { name, html: getObjectHTML(name) };
   });
   Object.keys(objectTypes).forEach((key) => {
     objectTypes[key].html.addEventListener('click', () => {
       selectedObject = objectTypes[key].name;
+      updateRightControl();
     });
   });
-  initControls();
+  initRightControl();
+  initDownControl();
   initScreen();
+}
+
+function main() {
+  init();
 }
 
 main();
